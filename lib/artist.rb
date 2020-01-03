984743d234dd569b3526e711c85cb662d657bcf9
Angelica  Thompson <i.thompson89@outlook.com>
2020-01-03 01:28:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,13 +11,12 @@ class Artist
   
   def songs
   # @songs
-   Song.all.select {|song|song.artist = self}
+   Song.all #.select {|song|song.artist = self}
   end 
   
   def add_song(song)
-    
     @songs << song.artist = self 
-    
    end  
+   
   
 end 
